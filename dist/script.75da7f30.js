8f154374620ddd0416664a19c9241ba4e294ed51
Chen He <chenhe.elec@gmail.com>
2019-09-14 16:34:59 +1000

    third commit, and will update some structure & pseudocode tonight

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -118,33 +118,87 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"script.js":[function(require,module,exports) {
-// overall structure:
-// 1. main
-//   - navbar
-//   - header
-//   - add new task
-//   - Calendar(optional)
-//   - main table
-// 2. footer
-// ---------------------------------------
-// add new task 
-var myTask = document.getElementById("myTask");
-var addNew = document.getElementById("addNew");
-var close = document.getElementsByClassName("close")[0];
+// add new todo function
+// document.write(Date());
+// const todoList = function() {
+//     var item = document.getElementById("todoInput").value;
+//     var text = document.createTextNode(item);
+//     var newItem = document.createElement("li");
+//     newItem.appendChild(text);
+//     document.getElementById("List").appendChild(newItem);
+// }
+// let id = 0
+$(document).ready(function () {
+  //add new task 
+  $("#form").submit(function (event) {
+    var contentValue = $("#taskContentInput").val(); // $("#newTaskContent").html(`${contentValue}`);
 
-addNew.onclick = function () {
-  myTask.style.display = "block";
-};
+    var categoryValue = $("#taskCategoryInput").val(); // $("#newTaskCategory").html(`${categoryValue}`);
 
-close.onclick = function () {
-  myTask.style.display = "none";
-};
+    var startDateValue = $("#taskStartDateInput").val(); // $("#newTaskStartDate").html(` ${startDateValue}`);
 
-window.onclick = function (event) {
-  if (event.target == myTask) {
-    myTask.style.display = "none";
-  }
-}; // ---------------------------------------
+    var dueDateValue = $("#taskDueDateInput").val(); // $("#newTaskDueDate").html(`${dueDateValue}`);
+
+    var prioirtyValue = $("#taskPriorityInput").val(); // $("#newTaskPriority").html(`${prioirtyValue}`);
+
+    var $row = $("<tr>").append("\n            <td>".concat(contentValue, "</td>\n            <td>").concat(categoryValue, "</td>\n            <td>").concat(startDateValue, "</td>\n            <td>").concat(dueDateValue, "</td>\n            <td>").concat(prioirtyValue, "</td>\n            <td><input type=\"checkbox\"></button></td>\n            <td><button class=\"btn btn-primary\">Update</button></td>\n            <td><button class=\"delete btn btn-primary\">Delete</button></td>\n        "));
+
+    if (dueDateValue === '') {
+      alert("Good, you need to ".concat(contentValue, " now!"));
+    } else {
+      alert("Good, you need to ".concat(contentValue, " now\nRemember to finish that before ").concat(dueDateValue, "!"));
+    }
+
+    ; // if (contentValue === '') {
+    //     alert("You must write something!");
+    // }
+
+    $('#myTable').append($row); // var deleteButton =<button>&times;</button>;
+    // $("ul").eq(0).append(value)
+
+    event.preventDefault();
+  }); //alert when add a new task
+  // $("#submitNewTask").click(function () {
+  //     alert(`Good, you got something need to do now\nRemember to finish that before the due day!`);
+  //     // $("#completeCheck").append('<li><button id="checkBox">Checkbox</button></li>');
+  //     // $("#updateButton").append('<li><button class="btn btn-primary">Update</button></li>');
+  //     // $("#deleteButton").append('<li><button class="btn btn-primary">Delelte</button></li>');
+  // });
+
+  $("#myTable").on('click', '.delete', function () {
+    var readyToDelete = confirm("Do you want to delete this task?");
+
+    if (readyToDelete === true) {
+      $(this).closest('tr').remove();
+    }
+  });
+  $(document).on('click', 'input[type="checkbox"]', function () {
+    if ($(this).prop("checked") == true) {
+      $(this).closest("tr").css("text-decoration", "line-through");
+    } else if ($(this).prop("checked") == false) {
+      $(this).closest("tr").css("text-decoration", "none");
+    }
+  }); // $("button").click(function () {
+  //     $("#div1").remove();
+  // });
+  // $("#checkBox").click(function () {
+  //     $("h1").css("background-color", "red");
+  // })
+  //search function 
+
+  $("#searchMyInput").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+    event.preventDefault();
+  }); // $('.delete').on('click', function(){
+  //     console.log('hello')
+  //     $(this).closest('tr').remove(); 
+  // })  
+}); //document.ready
+// todoList();
+// Add products to <table>
 },{}],"../../../../../../usr/local/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -173,7 +227,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62210" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55350" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- script.js
+++ script.js
@@ -1,27 +1,106 @@
+// add new todo function
+// document.write(Date());
 
-// overall structure:
-// 1. main
-//   - navbar
-//   - header
-//   - add new task
-//   - Calendar(optional)
-//   - main table
-// 2. footer
+// const todoList = function() {
+//     var item = document.getElementById("todoInput").value;
+//     var text = document.createTextNode(item);
+//     var newItem = document.createElement("li");
+//     newItem.appendChild(text);
+//     document.getElementById("List").appendChild(newItem);
+// }
+// let id = 0
+$(document).ready(function () {
+  //add new task 
+  $("#form").submit(function (event) {
 
-// ---------------------------------------
-// add new task 
-var myTask = document.getElementById("myTask");
-var addNew = document.getElementById("addNew");
-var close = document.getElementsByClassName("close")[0];
-addNew.onclick = function() {
-  myTask.style.display = "block";
-}
-close.onclick = function() {
-  myTask.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == myTask) {
-    myTask.style.display = "none";
-  }
-}
-// ---------------------------------------
+    var contentValue = $("#taskContentInput").val();
+    // $("#newTaskContent").html(`${contentValue}`);
+
+    var categoryValue = $("#taskCategoryInput").val();
+    // $("#newTaskCategory").html(`${categoryValue}`);
+
+    var startDateValue = $("#taskStartDateInput").val();
+    // $("#newTaskStartDate").html(` ${startDateValue}`);
+
+    var dueDateValue = $("#taskDueDateInput").val();
+    // $("#newTaskDueDate").html(`${dueDateValue}`);
+
+    var prioirtyValue = $("#taskPriorityInput").val();
+    // $("#newTaskPriority").html(`${prioirtyValue}`);
+
+    let $row = $("<tr>").append(`
+            <td>${contentValue}</td>
+            <td>${categoryValue}</td>
+            <td>${startDateValue}</td>
+            <td>${dueDateValue}</td>
+            <td>${prioirtyValue}</td>
+            <td><input type="checkbox"></button></td>
+            <td><button class="btn btn-primary">Update</button></td>
+            <td><button class="delete btn btn-primary">Delete</button></td>
+        `)
+    if (dueDateValue === '') {
+      alert(`Good, you need to ${contentValue} now!`);
+    } else {
+      alert(`Good, you need to ${contentValue} now\nRemember to finish that before ${dueDateValue}!`);
+    };
+    // if (contentValue === '') {
+    //     alert("You must write something!");
+    // }
+
+    $('#myTable').append($row)
+
+    // var deleteButton =<button>&times;</button>;
+    // $("ul").eq(0).append(value)
+    event.preventDefault();
+  });
+  //alert when add a new task
+  // $("#submitNewTask").click(function () {
+  //     alert(`Good, you got something need to do now\nRemember to finish that before the due day!`);
+  //     // $("#completeCheck").append('<li><button id="checkBox">Checkbox</button></li>');
+  //     // $("#updateButton").append('<li><button class="btn btn-primary">Update</button></li>');
+  //     // $("#deleteButton").append('<li><button class="btn btn-primary">Delelte</button></li>');
+  // });
+  $("#myTable").on('click', '.delete', function () {
+    var readyToDelete = confirm(`Do you want to delete this task?`);
+    if (readyToDelete === true) {
+      $(this).closest('tr').remove();
+    }
+
+  });
+  $(document).on('click', 'input[type="checkbox"]', function () {
+    if ($(this).prop("checked") == true) {
+      $(this).closest("tr").css("text-decoration", "line-through");
+    }
+    else if ($(this).prop("checked") == false) {
+      $(this).closest("tr").css("text-decoration", "none");
+    }
+  });
+
+
+  // $("button").click(function () {
+  //     $("#div1").remove();
+  // });
+  // $("#checkBox").click(function () {
+  //     $("h1").css("background-color", "red");
+  // })
+  //search function 
+  $("#searchMyInput").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+    event.preventDefault();
+  });
+
+  // $('.delete').on('click', function(){
+  //     console.log('hello')
+  //     $(this).closest('tr').remove(); 
+  // })  
+}); //document.ready
+
+
+
+
+
+// todoList();
+// Add products to <table>
