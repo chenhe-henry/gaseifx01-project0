5afea62566261177db0f9f25e314ab6f9946408b
Chen He <chenhe.elec@gmail.com>
2019-09-23 21:30:47 +1000

    update username

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+//set a global variable to help locating the input id and td 
 var numberOfRows = 0;
 var numberOfEditingRowNumber = 0;
 
 $(document).ready(function () {
+  //add new task 
   $("#dataEnterForm").submit(function (event) {
+    // get all values that entered
     var initialContentValue = $("#taskContentInput").val();
     var initialCategoryValue = $("#taskCategoryInput").val();
     var initialStartDateValue = $("#taskStartDateInput").val();
     var initialDueDateValue = $("#taskDueDateInput").val();
     var initialPrioirtyValue = $("#taskPriorityInput").val();
     
+    // create a new row everytime when add a new task, with basic details, a checkbox, a edit button and a delete button
     let $row = $("<tr>").append(`
             <td><i class="highlight far fa-star"></i></td>
             <td class="contentValue"><div id="initialContentValue${numberOfRows}">${initialContentValue}</div></td>
@@ -21,6 +25,7 @@ $(document).ready(function () {
             <td><button class="delete btn btn-primary">Delete</button></td>
         `)
 
+    // the task content cannot be empty, or it will show a alert
     if (initialContentValue !== '') {
       $('#myTable').append($row)
     } else { 
@@ -28,23 +33,29 @@ $(document).ready(function () {
     }
     event.preventDefault();
 
+    
+    // the input field will be empty after adding new task, so that ready for another task
     $("#taskContentInput").val("");
     $("#taskCategoryInput").val("");
     $("#taskStartDateInput").val("");
     $("#taskDueDateInput").val("");
     $("#taskPriorityInput").val("");
   });
-
+  //end of #dataEnterForm JQuery
+  
+  //the delete button function, every time delete the current row
   $("#myTable").on('click', '.delete', function () {
     var readyToDelete = confirm(`Do you want to delete this task?`);
     if (readyToDelete === true) {
       $(this).closest('tr').remove();
     }
   });
+  //end of delete button JQuery
+ 
 
-
-
+  // the edit function, let the input field to collect the td value in different rows
   $(document).on('click', '.edit', function (event) {
+    // convert the event.target.id from a string to a num
     const i = parseInt(event.target.id) + 1;
     numberOfEditingRowNumber = i;
 
@@ -53,8 +64,10 @@ $(document).ready(function () {
     $("#taskStartDateInput").val($("#initialStartDateValue" + (i - 1)).text());
     $("#taskDueDateInput").val($("#initialDueDateValue" + (i - 1)).text());
     $("#taskPriorityInput").val($("#initialPrioirtyValue" + (i - 1)).text());
+
   });
 
+  //the update function, put the input field value back into the table, and clean up the input field after updating
   $(document).on('click', '.update', function () { 
     $("#initialContentValue" + (numberOfEditingRowNumber - 1)).text($("#taskContentInput").val())
     $("#initialCategoryValue" + (numberOfEditingRowNumber - 1)).text($("#taskCategoryInput").val());
@@ -69,6 +82,7 @@ $(document).ready(function () {
     $("#taskPriorityInput").val("");
   });
 
+  //checkbox function, when ticked, the whole row will be line-through
   $(document).on('click', 'input[type="checkbox"]', function () {
     if ($(this).prop("checked") == true) {
       $(this).closest("tr").css("text-decoration", "line-through");
@@ -78,7 +92,9 @@ $(document).ready(function () {
       $(this).closest("tr").css("background-color", "rgba(255, 255, 255, 0)");
     }
   });
- 
+  //end of checkbox JQuery
+
+  //there is a star before every task, double click to highlight, and click to back to the background-color
   $(document).on('click', '.highlight', function () {
     $(this).closest("tr").css("background-color", "rgba(255, 255, 255, 0)");
   });
@@ -86,6 +102,9 @@ $(document).ready(function () {
     $(this).closest("tr").css("background-color", "beige");
   });
 
+
+  //search function, it will search and keep the whole row includes the result display on the screen, 
+  // and won't do anything if there is no key words entered
   $("#searchMyInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function () {
@@ -93,13 +112,15 @@ $(document).ready(function () {
     });
     event.preventDefault(); 
   }); 
-
+  //double click the search input field, clear the input field
   $("#searchMyInput").on("dblclick", function () {
     $("#searchMyInput").val("");
     event.preventDefault();
   }); 
+  //end of search JQuery
 }); 
 
+//display current date and time
 setInterval("time.innerHTML=new Date()", 1000);
 
-
+// 
